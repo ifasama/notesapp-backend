e838fdacb47b8736d1adfe555e55322757cbd03e
Ifa <ifakarmidi@gmail.com>
2023-07-02 20:42:50 +0300

    create collaboration table

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,86 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class CollaborationsHandler {
+  constructor(collaborationsService, notesService, validator) {
+    this._collaborationsService = collaborationsService;
+    this._notesService = notesService;
+    this._validator = validator;
+
+    this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+  }
+
+  async postCollaborationHandler(req, h) {
+    try {
+      this._validator.validateCollaborationPayload(req.payload);
+      const { id: credentialId } = req.auth.credentials;
+      const { noteId, userId } = req.payload;
+
+      await this._notesService.verifyNoteOwner(noteId, credentialId);
+      const collaborationId = await this._collaborationsService.addCollaboration(noteId, userId);
+
+      const res = h.response({
+        status: 'success',
+        message: 'Kolaborasi berhasil ditambahkan',
+        data: {
+          collaborationId,
+        },
+      });
+      res.code(201);
+      return res;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const res = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        res.code(error.statusCode);
+        return res;
+      }
+
+      // SERVER ERROR
+      const res = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      res.code(500);
+      console.error(error);
+      return res;
+    }
+  }
+
+  async deleteCollaborationHandler(req, h) {
+    try {
+      this._validator.validateCollaborationPayload(req.payload);
+      const { id: credentialId } = req.auth.credentials;
+      const { noteId, userId } = req.payload;
+
+      await this._notesService.verifyNoteOwner(noteId, credentialId);
+      await this._collaborationsService.deleteCollaboration(noteId, userId);
+
+      return {
+        status: 'success',
+        message: 'Kolaborasi berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const res = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        res.code(error.statusCode);
+        return res;
+      }
+
+      // SERVER ERROR
+      const res = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      res.code(500);
+      console.error(error);
+      return res;
+    }
+  }
+}
+module.exports = CollaborationsHandler;
--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -70,7 +70,7 @@ class NotesHandler {
       const { id } = req.params;
       const { id: credentialId } = req.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -104,7 +104,7 @@ class NotesHandler {
       const { id } = req.params;
       const { id: credentialId } = req.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       await this._service.editNoteById(id, req.payload);
       return {
         status: 'success',
