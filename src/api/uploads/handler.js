0e4a2825e5024eb6036c9cb0ab7cdd50874b271f
ifasama <ifakarmidi@gmail.com>
2023-10-06 00:54:05 +0300

    feat: implementing local storage

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,48 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
+  }
+
+  async postUploadImageHandler(req, h) {
+    try {
+      const { data } = req.payload;
+      this._validator.validateImageHeader(data.hapi.headers);
+
+      const filename = await this._service.writeFile(data, data.hapi);
+
+      const res = h.response({
+        status: 'success',
+        data: {
+          fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
+        },
+      });
+      res.code(201);
+      return res;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const res = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        res.code(error.statusCode);
+        return res;
+      }
+
+      // SERVER ERROR
+      const res = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      res.code(500);
+      console.error(error);
+      return res;
+    }
+  }
+}
+
+module.exports = UploadsHandler;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path'); // untuk dipakai di storageService
 
 // notes
 const notes = require('./api/notes');
@@ -31,11 +33,18 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/storageService');
+const UploadsValidator = require('./validator/uploads');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  // path berisi nama direktori yaitu api/uploads/file/images
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -51,6 +60,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -108,6 +120,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
